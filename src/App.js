9b9f2ab63ba12dc980b4a0b35199d837c5502008
Amanda Stern <amanda.stern.73@gmail.com>
2023-02-17 15:02:38 -0500

    make BrokenClick component and write handleClick function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import BrokenClick from './BrokenClick/BrokenClick';
+// import Game from './Game/Game';
+// import Button from './Button/Button';
+// import BrokenClick from './BrokenClick/BrokenClick';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <BrokenClick />
     </div>
   );
 }
